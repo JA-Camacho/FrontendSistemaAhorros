6bd28f6ac7315d4bec257e1d519bf9193ebfb444
Jose Camacho <jcamachor1@est.ups.edu.ec>
2024-01-29 10:39:35 -0500

    Update components and assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { PrincipalComponent } from "./components/principal/principal.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { MenuComponent } from "./components/menu/menu.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FooterComponent, PrincipalComponent, NavbarComponent, MenuComponent]
 })
 export class AppComponent {
   title = 'FrontEnd1';
